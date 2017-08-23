70159371143cdfdd9aff6fee1048f5a6cccfeafb
Tyler Wilson <x@tylers-mbp.attlocal.net>
2017-08-23 17:17:26 -0400

    made it functional, still too basic

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,3 +3,36 @@ var wins = 0
 var lose = 0
 var guessLeft = 9
 var computerGuess = computerChoices [Math.floor (Math.random() * computerChoices.length)]
+
+document.onkeyup = function(event) {
+
+    if (event.key == computerGuess) {
+        guessLeft = 9;
+        wins++; 
+        alert ("You Win!")}
+    if (event.key != computerGuess) {
+        lose++;
+        guessLeft--;
+        alert ("You lose!");
+    if (guessLeft == 0) {
+        lose++;
+        alert ("You lose!");
+        guessLeft == 9;
+    }
+
+    }
+    document.getElementById("win").innerHTML = "wins " + wins
+    document.getElementById("lose").innerHTML = "lose "+ lose
+    document.getElementById("guess").innerHTML = "Guesses left "+ guessLeft 
+
+
+
+
+
+
+
+
+
+
+
+}
