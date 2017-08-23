81d15b0b1abb9ca962e6aa18481e798320684240
Tyler Wilson <x@tylers-mbp.attlocal.net>
2017-08-23 16:38:02 -0400

    defined variables, set up basic html, started script

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,5 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0
+var lose = 0
+var guessLeft = 9
+var computerGuess = computerChoices [Math.floor (Math.random() * computerChoices.length)]
